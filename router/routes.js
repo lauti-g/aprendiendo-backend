b8b8aac867d27e28c6008e65d0ac572de42983f6
lau_g <lautaro.gauna0102@gmail.com>
2025-08-03 21:43:56 -0300

    Le agrego autenticacion al CRUD hice una autenticacion propia, sin ningun paquete que me ayude en este hambito, solo imaginacion y muchos console.log jajjajaja, me costo mucho pero se logro, estuvo buenisimo y siento que aprendi un monton, ahora queda aprender a usar paquetes como jwt entre otras dependencias que me ayuden a mejorar y hacer autenticaciones seguramente mas seguras y mucho mas rapido y eficiente que lo que hice, igualmente siento que fue una buena practica por mas que mi codigo de seguro tenga mucho que mejorar y cuando lo vea en el futuro me voy a querer matar, fue una linda experiencia.

--- router/routes.js
+++ router/routes.js
@@ -20,24 +20,103 @@ router.get('/changeUsername', (req, res)=>{
 
 router.post('/registration',async (req, res)=>{
 	const newUser = req.body
-	pool.query(`INSERT INTO users (username, password, email, age) VALUES ("${newUser.username}", "${newUser.password}", "${newUser.email}", "${newUser.age}")`)
-	res.send("usuario registrado")
+	const username = newUser.username
+	const password = newUser.password
+	const confirmPassword = newUser.confirmPassword
+	const email = newUser.email
+	const age = newUser.age
+	const oldsUser = await  pool.query(`SELECT username FROM users WHERE username = "${username}"`)
+	const oldsEMails = await pool.query(`SELECT email FROM users WHERE email = "${email}"`)
+	try {
+	if(oldsUser[0][0] != undefined){
+		res.send("nombre de usuario en uso eleji otro")
+	}
+	else if(oldsEMails[0][0] != undefined) {
+		res.send("E-Mail en uso eleji otro")
+	}
+	else if(username.length < 3){
+    	res.send("nombre muy corto minimo 3 caracteres")
+		}
+	else if(password.length < 5){
+		res.send("contraceña muy corta minimo 5 caracteres")
+		}
+	else if(confirmPassword !== password){
+		res.send("confirm password tiene que ser igual al password")
+		}
+	else if(age < 18) {
+		res.send("edad minima requerida: 18")
+		}
+	else if(!email.includes("@")){
+		res.send("se requerie e-mail")
+		}
+	else if("number" != typeof(age) ){
+		res.send("edad tiene que ser un numero")
+		} 
+	else{
+    	await pool.query(`INSERT INTO users (username, password, email, age) VALUES ("${username}", "${password}", "${email}", "${age}")`)
+		res.send("usuario registrado")
+		}}
+	catch{
+		console.error(error);
+		res.send("ocurrio un error")
+	}
 })
 
 
 router.delete('/deleteUser',async (req,res)=>{
 	const deleteUser = req.body
-	console.log(deleteUser)
-	const idUser = await pool.query(`SELECT id FROM users WHERE  username = "${deleteUser.username}"`)
-	pool.query(`DELETE  FROM users WHERE id = ${idUser[0][0].id} `)
-	res.send(`usuario ${deleteUser.username} borrado`)
+	const username = deleteUser.username
+	const password = deleteUser.password
+	const getPassword = await pool.query(`SELECT password FROM users WHERE username = "${username}"`)
+	if(getPassword[0][0] == undefined){
+		res.send("el usuario no existe, asegurese de estar registrado.")
+	}
+	else if(getPassword[0][0].password !== password){
+		res.send("contraceña incorrecta")
+	}
+	else{
+		const idUser = await pool.query(`SELECT id FROM users WHERE  username = "${username}"`)
+		pool.query(`DELETE  FROM users WHERE id = ${idUser[0][0].id} `)
+		res.send(`usuario ${username} borrado`)
+	}
+
+
+
+
+
+
+
+
+
+
+	
 })
 
 router.put('/changeUsername', async (req, res)=>{
-	const newUsername = req.body
-	const id = await pool.query(`SELECT id FROM users WHERE username = "${newUsername.username}"`)
-	pool.query(`UPDATE users SET username = "${newUsername.newUsername}" WHERE id = ${id[0][0].id}`)
-	res.end("username updated")
-})
+	const changeUsername = req.body
+	const username = changeUsername.username
+	const newUsername = changeUsername.newUsername
+	const password = changeUsername.password
+	const getPassword = await pool.query(`SELECT password FROM users WHERE username = "${username}"`)
+	const getUsers = await pool.query(`SELECT username FROM users WHERE username = "${newUsername}"`)
+	if(getPassword[0][0] !== undefined){
+		if(getUsers[0][0] !== undefined){
+			res.send("este nombre de usuario ya esta en uso")
+		}
+		else if(newUsername.length < 3){
+			res.send("el nuevo nombre de usuario es muy corto minimo 3 caracteres")
+		}
+		else if(getPassword[0][0].password !== password){
+			res.send("contraceña incorrecta")
+		}
+		else{
+			const id = await pool.query(`SELECT id FROM users WHERE username = "${username}"`)
+			pool.query(`UPDATE users SET username = "${newUsername}" WHERE id = ${id[0][0].id}`)
+			res.send("nombre de usuario actualizados")
+		}
+	}else {res.send("nombre de usuario o contraceña incorrecta")}
+}
+)
 
 export default router
+
